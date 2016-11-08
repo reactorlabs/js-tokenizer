f85470877a0ad88011eff478db564d33eb3384e5
peta <peta.maj82@gmail.com>
2016-11-08 09:23:51 +0100

    Zero parses properly

--- src/tokenizers/js.cpp
+++ src/tokenizers/js.cpp
@@ -182,24 +182,26 @@ void JSTokenizer::numericLiteral() {
             while(not eof() and isHexDigit(top()))
                 pop(1);
             addToken(start);
+            return;
         } else if (peek(1) == 'o' or peek(1) == 'O') {
             pop(2);
             while(not eof() and isOctDigit(top()))
                 pop(1);
             addToken(start);
+            return;
         } else if (peek(1) == 'b' or peek(1) == 'B') {
             pop(2);
             while(not eof() and isBinDigit(top()))
                 pop(1);
             addToken(start);
+            return;
         }
-    } else {
-        while (not eof() and isDecDigit(top()))
-            pop(1);
-        numericLiteralFloatingPointPart();
-        numericLiteralExponentPart();
-        addToken(start);
     }
+    while (not eof() and isDecDigit(top()))
+        pop(1);
+    numericLiteralFloatingPointPart();
+    numericLiteralExponentPart();
+    addToken(start);
 }
 
 
